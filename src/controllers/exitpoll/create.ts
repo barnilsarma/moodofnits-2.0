9c58bfcb6a807d46f774384e1fbc668e25eb7a90
Samarjit25 <samarjitroy025@gmail.com>
2025-08-03 17:48:59 +0530

    exit poll controller

--- src/controllers/exitpoll/create.ts
+++ src/controllers/exitpoll/create.ts
@@ -1,1 +1,32 @@
 //controller for exitpoll
+import * as Interfaces from "../../interfaces";
+import { prisma } from "../../utils/index";
+
+const Create: Interfaces.Controllers.Async = async (req, res) => {
+  try {
+    const { positionIds } = req.body;
+
+    const exitPoll = await prisma.exitPoll.create({
+      data: {
+        positions: {
+          connect: positionIds.map((id: string) => ({ id })),
+        },
+      },
+      include: {
+        positions: true,
+      },
+    });
+
+    return res.json({
+      msg: "Exit poll created successfully",
+      data: exitPoll,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: "Error creating exit poll",
+      error,
+    });
+  }
+};
+
+export default Create;
