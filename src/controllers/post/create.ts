5da6baa660ca25faa22d843cd90b7a80c4eb80e2
rishinhk004 <rishirajsaha555@gmail.com>
2025-08-05 15:39:40 +0530

    fixes

--- src/controllers/post/create.ts
+++ src/controllers/post/create.ts
@@ -1,12 +1,36 @@
-import * as Interfaces from "../../interfaces";
-import { prisma } from "../../utils";
+import * as Utils from "src/utils";
+import * as Interfaces from "src/interfaces";
+import { prisma } from "src/utils";
 
-const Create: Interfaces.Controllers.Async = async (req, res) => {
+const Create: Interfaces.Controllers.Async = async (req, res, next) => {
   try {
-    const { type, question, title, description, likes, authorId } = req.body;
+    const authorizationHeader: string | undefined = req.headers.authorization;
+    if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+      return next(Utils.Response.error("Authorization token is required", 401));
+    }
+
+    const token: string = authorizationHeader.split(" ")[1];
+    const firebaseAuth = Utils.Firebase.getFirebaseAuth();
+
+    let decodedToken;
+    try {
+      decodedToken = await firebaseAuth.verifyIdToken(token);
+    } catch (err) {
+      console.error("Error verifying Firebase token:", err);
+      return next(Utils.Response.error("Invalid or expired token", 401));
+    }
+    const uid = decodedToken.uid;
+    const user = await prisma.user.findUnique({
+      where: { firebaseId: uid },
+    });
+    if (!user) {
+      return next(Utils.Response.error("User not found", 404));
+    }
+    const { type, question, title, description, likes } = req.body;
     const allowedTypes = ["PHOTO", "VIDEO", "TEXT", "POLL"];
+
     if (!allowedTypes.includes(type)) {
-      return res.status(400).json({ msg: "Invalid post type" });
+      return next(Utils.Response.error("Invalid post type", 400));
     }
     const asset = req.file && (req.file as Express.MulterS3.File).location;
     const post = await prisma.post.create({
@@ -16,21 +40,17 @@ const Create: Interfaces.Controllers.Async = async (req, res) => {
         title,
         description,
         likes: Number(likes) || 0,
-        asset: asset,
-        authorId,
+        asset,
+        authorId: user.id,
       },
     });
 
-    return res.json({
-      msg: "Post created successfully",
-      data: post,
-    });
-  } catch (error) {
-    console.error("Error creating post:", error);
-    return res.status(500).json({
-      msg: "Problem in creating post",
-      error: error instanceof Error ? error.message : error,
-    });
+    return res.json(
+      Utils.Response.success({ msg: "Post created successfully", post }, 201)
+    );
+  } catch (err) {
+    console.error("Error creating post:", err);
+    return next(Utils.Response.error((err as Error).message, 500));
   }
 };
 
