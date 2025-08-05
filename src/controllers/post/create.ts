d3850ab175e823e956bd4e438682b005fb45f854
rishinhk004 <rishirajsaha555@gmail.com>
2025-08-05 11:45:24 +0530

    fixes

--- src/controllers/post/create.ts
+++ src/controllers/post/create.ts
@@ -4,6 +4,10 @@ import { prisma } from "../../utils";
 const Create: Interfaces.Controllers.Async = async (req, res) => {
   try {
     const { type, question, title, description, likes, authorId } = req.body;
+    const allowedTypes = ["PHOTO", "VIDEO", "TEXT", "POLL"];
+    if (!allowedTypes.includes(type)) {
+      return res.status(400).json({ msg: "Invalid post type" });
+    }
     const asset = req.file && (req.file as Express.MulterS3.File).location;
     const post = await prisma.post.create({
       data: {
