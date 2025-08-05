2ea8746bb2a73651e90fc8a28bbfbca7fcc0adbe
rishinhk004 <rishirajsaha555@gmail.com>
2025-08-05 14:09:41 +0530

    fixes

--- src/globals/success/health.ts
+++ src/globals/success/health.ts
@@ -1,6 +1,6 @@
 import * as Utils from "../../utils";
 
-const api = Utils.Response.success("GDSC NITS API");
+const api = Utils.Response.success("Mood of Nits api!");
 
 // const pong = Utils.Response.success("Pong", 200);
 const pong = Utils.Response.success("Pong");
